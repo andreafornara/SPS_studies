3efe5e78ecfdded39fbdcb5d7fcf6c55dfee5338
andrew960 <andrew960@hotmail.it>
2024-01-17 13:47:59 +0100

    Removed old files

--- old_ones/install_me.sh
+++
@@ -1,8 +0,0 @@
-wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
-bash Miniforge3-Linux-x86_64.sh -b  -p ./miniforge -f
-source miniforge/bin/activate                                                    
-python -m pip install -r requirements.txt
-conda install mamba -n base -c conda-forge
-pip install cupy-cuda11x
-mamba install cudatoolkit=11.8.0
-pip install gpustat     
