77e6f3cf1c010ffe30d1f194b8dc0b5a59e1fefe
andrew960 <andrew960@hotmail.it>
2024-01-17 11:10:03 +0100

    installation not working, needs checking

--- old_ones/install_me.sh
+++ old_ones/install_me.sh
@@ -2,3 +2,7 @@ wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge
 bash Miniforge3-Linux-x86_64.sh -b  -p ./miniforge -f
 source miniforge/bin/activate                                                    
 python -m pip install -r requirements.txt
+conda install mamba -n base -c conda-forge
+pip install cupy-cuda11x
+mamba install cudatoolkit=11.8.0
+pip install gpustat     
